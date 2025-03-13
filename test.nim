6dabe683f59296a481d8305d707957d338354aee
gitsergio99 <chernikov1981sv@gmail.com>
2025-03-13 14:30:58 +0700

    modified:   test.nim 	modified:   ups_lib.nim

--- test.nim
+++ test.nim
@@ -1,3 +1,3 @@
 import ups_lib
 
-echo lowUpsRequest("L","10.39.225.52",4001)
+echo lowUpsRequest("t","10.39.225.52",4001)
--- ups_lib.nim
+++ ups_lib.nim
@@ -1,6 +1,25 @@
 import std/[strformat,net]
 
 type
+    Ups_cmd* = enum
+        fp_test = "A" #Front panel test
+        bat_voltage = "B" #Battery voltage
+        int_temp = "C" #Internal temperature
+        runt_calib = "D" #Runtime calibration
+        auto_self_test_int = "E" #Automatic self test intervals
+        line_freq = "F" #Line freq. Hz
+        cause_of_transf = "G" #Cause of transfer
+        in_line_voltage = "L" #Input line voltage
+        max_line_voltage = "M" #Maximum line voltage received since last M query
+        min_line_voltage = "N" #Minimum line voltage received since last N query
+        out_voltage = "O" #Output voltage
+        power_load = "P" #Power load %
+        status_flags = "Q" #Status flags
+        self_test = "W" #Do self test of battery store in X
+        res_self_test = "X"  #Result of selftest
+        batt_level = "f" #Battery level %
+        
+type
     Ups* = object
         ip_adress*: string
         name*:string
